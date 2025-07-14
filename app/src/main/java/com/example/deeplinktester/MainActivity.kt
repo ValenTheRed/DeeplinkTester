5bcf8ac329c1975e457a7a8f6e6f875872247c04
Pranjal Soni <soni.pranjals17@gmail.com>
2025-07-14 22:46:17 +0530

    Feat: open deeplink

--- app/src/main/java/com/example/deeplinktester/MainActivity.kt
+++ app/src/main/java/com/example/deeplinktester/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.deeplinktester
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,19 +24,30 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.deeplinktester.ui.theme.DeeplinkTesterTheme
+import com.example.deeplinktester.utils.debounce
+import kotlinx.coroutines.launch
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
     name = "history"
@@ -56,20 +69,32 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun App() {
+    val snackbarHostState = remember { SnackbarHostState() }
+    val scope = rememberCoroutineScope()
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = {
             TopAppBar(
                 title = { Text("Deeplink Tester") },
             )
-        }
+        },
+        snackbarHost = { SnackbarHost(snackbarHostState) }
     ) { innerPadding ->
         Column(
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(horizontal = 16.dp)
         ) {
-            Input()
+            Input(
+                showSnackbar = {message ->
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = message
+                        )
+                    }
+                }
+            )
             HorizontalDivider(
                 modifier = Modifier.padding(vertical = 12.dp)
             )
@@ -84,20 +109,35 @@ fun App() {
 
 @Composable
 fun Input(
+    showSnackbar: (message: String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    var value by remember { mutableStateOf("") }
+    val ctx = LocalContext.current
+
     Column(
         modifier = modifier,
     ) {
         TextField(
-            value = "",
-            onValueChange = {},
+            value = value,
+            onValueChange = { text ->
+                value = text
+            },
             placeholder = { Text("deeplink://") },
             modifier = Modifier.fillMaxWidth(),
         )
         Spacer(Modifier.height(8.dp))
         Button(
-            onClick = {},
+            onClick = debounce(400, {
+                if (value.trim() == "") {
+                    return@debounce
+                }
+                try {
+                    ctx.startActivity(Intent(Intent.ACTION_VIEW, value.toUri()))
+                } catch (_: ActivityNotFoundException) {
+                    showSnackbar("No app exists for the deeplink")
+                }
+            }),
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Open Deeplink")
---
+++ app/src/main/java/com/example/deeplinktester/utils/Utils.kt
@@ -0,0 +1,25 @@
+package com.example.deeplinktester.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+/**
+ * A immediate/leading debounce implementation.
+ */
+@Composable
+fun debounce(
+    intervalMillis: Long,
+    onClick: () -> Unit,
+): () -> Unit {
+    var lastTimeMillis: Long by remember { mutableLongStateOf(0L) }
+    return {
+        val currentMillis = System.currentTimeMillis()
+        if ((currentMillis - lastTimeMillis) >= intervalMillis) {
+            onClick()
+        }
+        lastTimeMillis = currentMillis
+    }
+}
