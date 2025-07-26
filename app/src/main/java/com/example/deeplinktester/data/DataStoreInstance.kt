77df32c2bac2e0ba17b1f5455bd2ab8fe23efaea
Pranjal Soni <soni.pranjals17@gmail.com>
2025-07-26 12:20:23 +0530

    Chore: remove unused methods and imports

--- app/src/main/java/com/example/deeplinktester/data/DataStoreInstance.kt
+++ app/src/main/java/com/example/deeplinktester/data/DataStoreInstance.kt
@@ -3,14 +3,8 @@ package com.example.deeplinktester.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
-import com.example.deeplinktester.ui.HistoryUiState
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.single
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 object DataStoreInstance {
     val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
@@ -18,18 +12,4 @@ object DataStoreInstance {
     )
 
     val HISTORY_LIST = stringPreferencesKey("history")
-
-    suspend fun saveHistory(ctx: Context, uiState: HistoryUiState) {
-        ctx.dataStore.edit { store ->
-            store[HISTORY_LIST] = Json.encodeToString(uiState)
-        }
-    }
-
-    suspend fun fetchHistory(ctx: Context): HistoryUiState? {
-        return ctx.dataStore.data.map { store ->
-            store[HISTORY_LIST]?.let {
-                Json.decodeFromString<HistoryUiState>(it)
-            }
-        }.single()
-    }
 }
