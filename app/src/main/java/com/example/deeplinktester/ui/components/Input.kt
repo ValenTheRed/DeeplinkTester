ba96e6fbc69881427c34a5a7396dfcf59e23db38
Pranjal Soni <soni.pranjals17@gmail.com>
2026-01-30 00:55:38 +0530

    Refactor: add an icon to open deeplink button

--- app/src/main/java/com/example/deeplinktester/ui/components/Input.kt
+++ app/src/main/java/com/example/deeplinktester/ui/components/Input.kt
@@ -3,9 +3,11 @@ package com.example.deeplinktester.ui.components
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -22,8 +24,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.core.net.toUri
@@ -101,7 +105,16 @@ fun Input(
             modifier = Modifier.fillMaxWidth(),
             shape = MaterialTheme.shapes.large
         ) {
-            Text(stringResource(R.string.open_deeplink))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(stringResource(R.string.open))
+                Spacer(Modifier.width(Density.ExtraSmall))
+                Icon(
+                    painterResource(R.drawable.go),
+                    stringResource(R.string.open_deeplink)
+                )
+            }
         }
     }
 }
