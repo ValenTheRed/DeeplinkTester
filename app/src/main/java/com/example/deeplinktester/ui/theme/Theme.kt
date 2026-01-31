bea264e4eb6900c4eae96537730f0a3b76c85ffc
Pranjal Soni <soni.pranjals17@gmail.com>
2026-01-31 16:27:10 +0530

    Feat: use Fira Sans as default font

--- app/src/main/java/com/example/deeplinktester/ui/theme/Theme.kt
+++ app/src/main/java/com/example/deeplinktester/ui/theme/Theme.kt
@@ -1,15 +1,19 @@
 package com.example.deeplinktester.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.deeplinktester.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -33,6 +37,34 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+private val FiraSans = FontFamily(
+//    Font(R.font.fira_sans_light, weight = FontWeight.Light),
+    Font(R.font.fira_sans, weight = FontWeight.Normal),
+    Font(R.font.fira_sans_medium, weight = FontWeight.Medium),
+//    Font(R.font.fira_sans_semibold, weight = FontWeight.SemiBold),
+//    Font(R.font.fira_sans_bold, weight = FontWeight.Bold)
+)
+
+private val FiraSansTypography = Typography().run {
+    copy(
+        displayLarge = displayLarge.copy(fontFamily = FiraSans),
+        displayMedium = displayMedium.copy(fontFamily = FiraSans),
+        displaySmall = displaySmall.copy(fontFamily = FiraSans),
+        headlineLarge = headlineLarge.copy(fontFamily = FiraSans),
+        headlineMedium = headlineMedium.copy(fontFamily = FiraSans),
+        headlineSmall = headlineSmall.copy(fontFamily = FiraSans),
+        titleLarge = titleLarge.copy(fontFamily = FiraSans),
+        titleMedium = titleMedium.copy(fontFamily = FiraSans),
+        titleSmall = titleSmall.copy(fontFamily = FiraSans),
+        bodyLarge = bodyLarge.copy(fontFamily =  FiraSans),
+        bodyMedium = bodyMedium.copy(fontFamily = FiraSans),
+        bodySmall = bodySmall.copy(fontFamily = FiraSans),
+        labelLarge = labelLarge.copy(fontFamily = FiraSans),
+        labelMedium = labelMedium.copy(fontFamily = FiraSans),
+        labelSmall = labelSmall.copy(fontFamily = FiraSans)
+    )
+}
+
 @Composable
 fun DeeplinkTesterTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -52,6 +84,7 @@ fun DeeplinkTesterTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        content = content
+        content = content,
+        typography = FiraSansTypography
     )
 }
