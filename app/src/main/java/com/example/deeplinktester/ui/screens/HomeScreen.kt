56c14c32114df3e443176006a56524ea251d9bbc
Pranjal Soni <soni.pranjals17@gmail.com>
2026-01-31 17:56:00 +0530

    Feat: use a custom app bar

--- app/src/main/java/com/example/deeplinktester/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/deeplinktester/ui/screens/HomeScreen.kt
@@ -1,17 +1,24 @@
 package com.example.deeplinktester.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.collectAsState
@@ -19,9 +26,11 @@ import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
 import com.example.deeplinktester.R
 import com.example.deeplinktester.ui.HomeModel
@@ -29,6 +38,7 @@ import com.example.deeplinktester.ui.SnackbarController
 import com.example.deeplinktester.ui.components.HistoryItem
 import com.example.deeplinktester.ui.components.HistoryList
 import com.example.deeplinktester.ui.components.Input
+import com.example.deeplinktester.ui.theme.AppEdgeType
 import com.example.deeplinktester.ui.theme.Density
 import com.example.deeplinktester.ui.theme.appEdgePadding
 
@@ -57,20 +67,36 @@ fun HomeScreen(
                 Column(
                     modifier = Modifier.padding(bottom = Density.Small)
                 ) {
-                    TopAppBar(
-                        title = { Text(stringResource(R.string.app_name)) },
-                        actions = {
-                            IconButton(
-                                onClick = { navHostController.navigate("search") },
-                            ) {
-                                Icon(
-                                    painter = painterResource(R.drawable.search),
-                                    contentDescription = stringResource(R.string.search_deeplinks),
-                                )
-                            }
-
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        modifier = Modifier
+                            .background(
+                                MaterialTheme.colorScheme
+                                    .primaryContainer,
+                            )
+                            .fillMaxWidth()
+                            .statusBarsPadding()
+                            .appEdgePadding(pad = AppEdgeType.Start)
+                            .padding(top = Density.Medium),
+                    ) {
+                        Text(
+                            stringResource(R.string.app_name),
+                            modifier = Modifier.weight(1f),
+                            style = MaterialTheme.typography.titleLarge,
+                            fontWeight = FontWeight.Medium
+                        )
+                        IconButton(
+                            onClick = { navHostController.navigate("search") },
+                        ) {
+                            Icon(
+                                painter = painterResource(R.drawable.search),
+                                contentDescription = stringResource(R.string.search_deeplinks),
+                            )
                         }
-                    )
+
+                    }
+                    Spacer(Modifier.height(Density.Medium))
                     Input(
                         onOpenDeeplink = { deeplink ->
                             homeModel.push(deeplink)
